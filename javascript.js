e74c6760edabe2bcd1a3eb94316aeee1993dde89
Brendan Ching <brendanching1@gmail.com>
2024-05-29 22:09:21 +0000

    fixed some issues with js for game

--- javascript.js
+++ javascript.js
@@ -58,20 +58,10 @@
 
 
 
-let questionImages = {
-    Cirrus: "assets/images/cirrus6_big.jpg",
-    Cirrocumulus: "assets/images/cirrocumulus1_big.jpg",
-    Cirrostrattus: "assets/images/cirrostratus1_big.jpg",
-    Altcomulus: "assets/images/altocumulus1_big.jpg",
-    Altostratus: "assets/images/altostratus3_big.jpg",
-    Stratus: "assets/images/stratus2_big.jpg",
-    Stratocumulus: "assets/images/stratocumulus1_big.jpg",
-    Nimbostratus: "assets/images/nimbostratus1_big.jpg",
-    Cumulus: "assets/images/cumulus3_big.jpg",
-    Cumulonimbus: "assets/images/cumulonimbus3_big.jpg"
-};
 
-let arrayquestionImages = questionImages.entries();
+let arrayquestionImages = ["assets/images/cirrus6_big.jpg", "assets/images/cirrocumulus1_big.jpg", "assets/images/cirrostratus1_big.jpg", "assets/images/altocumulus1_big.jpg",
+"assets/images/altostratus3_big.jpg", "assets/images/stratus2_big.jpg", "assets/images/stratocumulus1_big.jpg", "assets/images/nimbostratus1_big.jpg", "assets/images/cumulus3_big.jpg",
+"assets/images/cumulonimbus3_big.jpg"]
 
 let correctAnswers = ["Cirrus", "Cirrocumulus", "Cirrostrattus", "Altcomulus", "Altostratus", "Stratus", "Stratocumulus", "Nimbostratus", "Cumulus", "Cumulonimbus"];
 
@@ -82,9 +72,9 @@ let optionsArray = ["option0", "option1", "option2", "option3"];
 // and it changes the HTML of the next-question button when the user is on the last question
 let i = 0;
 function questionCounter() {
-    let i = i + 1;
+    i = i + 1;
     if (i === 9){
-    next-question.innerHTML = '<a href="finishing-page.html">Finish Quiz!</a>'
+    document.getElementById('next-question').innerHTML = '<a href="finishing-page.html">Finish Quiz!</a>';
     };
     return i;
 }
@@ -95,33 +85,48 @@ function whichQuestion(){
     document.getElementById("image-question").src = arrayquestionImages[i];
 }
 
+document.getElementById("startQuiz").addEventListener('click', whichQuestion);
+document.getElementById("startQuiz").addEventListener('click', correctAnswerPosition);
+document.getElementById("startQuiz").addEventListener('click', randomAnswers0);
+document.getElementById("startQuiz").addEventListener('click', randomAnswers1);
+document.getElementById("startQuiz").addEventListener('click', randomAnswers2);
+document.getElementById("startQuiz").addEventListener('click', randomAnswers3);
 
-let randomPosition = Math.floor(Math.random()*4);
+document.getElementById("next-question").addEventListener('click', whichQuestion);
+document.getElementById("next-question").addEventListener('click', correctAnswerPosition);
+document.getElementById("next-question").addEventListener('click', randomAnswers0);
+document.getElementById("next-question").addEventListener('click', randomAnswers1);
+document.getElementById("next-question").addEventListener('click', randomAnswers2);
+document.getElementById("next-question").addEventListener('click', randomAnswers3);
+document.getElementById("next-question").addEventListener('click', questionCounter);
 
 //this function puts the correct answer in one of the options boxes
+
 function correctAnswerPosition(){
+    let randomPosition = Math.floor(Math.random()*4);
     if (randomPosition === 0){
-        getElementById("option0").innerText = correctAnswers[i];
+        document.getElementById("option0").innerText = correctAnswers[i];
     } else if (randomPosition === 1){
-        getElementById("option1").innerText = correctAnswers[i];
+        document.getElementById("option1").innerText = correctAnswers[i];
     } else if (randomPosition === 2){
-        getElementById("option2").innerText = correctAnswers[i];
+        document.getElementById("option2").innerText = correctAnswers[i];
     } else if (randomPosition === 3){
-        getElementById("option3").innerText = correctAnswers[i];
+        document.getElementById("option3").innerText = correctAnswers[i];
     }
 }
 
 //functions for randomising the positions of incorrect answers
 function randomAnswers0() {
-        if (getElementById("option0").innerText === ""){
-            getElementById("option0").innerText = correctAnswers[Math.floor(Math.random()*10)];
+        if (document.getElementById("option0").innerText === ""){
+            document.getElementById("option0").innerText = correctAnswers[Math.floor(Math.random()*10)];
+            
         } 
 }
 
 function randomAnswers1() {
     k = Math.floor(Math.random()*9);
-    if (getElementById("option1").innerHTML === ""){
-        getElementById("option1").innerHTML = correctAnswers[k];
+    if (document.getElementById("option1").innerHTML === ""){
+        document.getElementById("option1").innerHTML = correctAnswers[k];
         correctAnswers.splice(k, 1);
     } 
 
@@ -129,16 +134,16 @@ function randomAnswers1() {
 
 function randomAnswers2() {
     k = Math.floor(Math.random()*8)
-    if (getElementById("option2").innerHTML === ""){
-        getElementById("option2").innerHTML = correctAnswers[k];
+    if (document.getElementById("option2").innerHTML === ""){
+        document.getElementById("option2").innerHTML = correctAnswers[k];
         correctAnswers.splice(k, 1);
     } 
 }
 
 function randomAnswers3() {
     k = Math.floor(Math.random()*7)
-    if (getElementById("option3").innerHTML === ""){
-        getElementById("option3").innerHTML = correctAnswers[k];
+    if (document.getElementById("option3").innerHTML === ""){
+        document.getElementById("option3").innerHTML = correctAnswers[k];
 }
 }
 function resetArray() {
@@ -146,21 +151,22 @@ function resetArray() {
 }
 
 //this function resets the eventlisteners after the next-question button is clicked
-getElementById("next-question").addEventListener("click", resetEventListeners);
-function resetEventListeners {
-getElementById("next-question").addEventListener("click", randomAnswers0);
-getElementById("next-question").addEventListener("click", randomAnswers1);
-getElementById("next-question").addEventListener("click", randomAnswers2);
-getElementById("next-question").addEventListener("click", randomAnswers3);
+document.getElementById("next-question").addEventListener("click", resetEventListeners);
+function resetEventListeners() {
+document.getElementById("next-question").addEventListener("click", randomAnswers0);
+document.getElementById("next-question").addEventListener("click", randomAnswers1);
+document.getElementById("next-question").addEventListener("click", randomAnswers2);
+document.getElementById("next-question").addEventListener("click", randomAnswers3);
 }
 
 //These functions check each option for correctness and add to either the correct or incorrect answers tally
 let correctlyAnswered = 0;
 let incorrectlyAnswered = 0;
 function checkAnswer0(){
-    getElementById("option0").removeEventListener("click", checkAnswer3)
-    if (getElementById("option0").innerText === correctAnswers[i]){
+    document.getElementById("option0").removeEventListener("click", checkAnswer3);
+    if (document.getElementById("option0").innerText === correctAnswers[i]){
         correctlyAnswered = correctlyAnswered + 1;
+
         return correctlyAnswered;
     } else {
         incorrectlyAnswered = incorrectlyAnswered + 1;
@@ -169,8 +175,8 @@ function checkAnswer0(){
 }
 
 function checkAnswer1(){
-    getElementById("option1").removeEventListener("click", checkAnswer3)
-    if (getElementById("option1").innerText === correctAnswers[i]){
+    document.getElementById("option1").removeEventListener("click", checkAnswer3);
+    if (document.getElementById("option1").innerText === correctAnswers[i]){
         correctlyAnswered = correctlyAnswered + 1;
         return correctlyAnswered;
     } else {
@@ -180,8 +186,8 @@ function checkAnswer1(){
 }
 
 function checkAnswer2(){
-    getElementById("option2").removeEventListener("click", checkAnswer3)
-    if (getElementById("option2").innerText === correctAnswers[i]){
+    document.getElementById("option2").removeEventListener("click", checkAnswer3);
+    if (document.getElementById("option2").innerText === correctAnswers[i]){
         correctlyAnswered = correctlyAnswered + 1;
         return correctlyAnswered;
     } else {
@@ -191,8 +197,8 @@ function checkAnswer2(){
 }
 
 function checkAnswer3(){
-    getElementById("option3").removeEventListener("click", checkAnswer3)
-    if (getElementById("option3").innerText === correctAnswers[i]){
+    document.getElementById("option3").removeEventListener("click", checkAnswer3)
+    if (document.getElementById("option3").innerText === correctAnswers[i]){
         correctlyAnswered = correctlyAnswered + 1;
         return correctlyAnswered;
     } else {
@@ -201,11 +207,18 @@ function checkAnswer3(){
     }
 }
 
+document.getElementById("next-question").addEventListener("click", resetArray);
 
-next-question.addEventListener("click", resetArray);
+function scoreTally(){
+    
+}
+
+
+
+//Sian's
 // Prevent form from submitting normally
 document.getElementById('usernameForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
+    event.preventDefault();
 
 
 // Get the username from the input field
