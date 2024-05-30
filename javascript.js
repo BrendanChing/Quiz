7f3eb28660d6c5c6119115a273c98201dfa8369e
Sian Jones <sianwroejones@gmail.com>
2024-05-30 09:27:32 +0000

    js wip

--- javascript.js
+++ javascript.js
@@ -1,176 +1,97 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const page = document.body.getAttribute('data-page');
+    alert(`Hello your page is ${page}`)
 
-//Sian's
-// Prevent form from submitting normally
-document.getElementById('usernameForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+    if (page === 'index') {
+        console.log(`This is Page 1: ${page}`);
+        // Prevent form from submitting normally
+        document.getElementById('usernameForm').addEventListener('submit', function(event) {
+            event.preventDefault();
 
 
-// Get the username from the input field
-const username =
-document.getElementById('username').value;
+            // Get the username from the input field
+            const username = document.getElementById('username').value;
 
-// Store the username in localStorage for use throughout the quiz
-localStorage.setItem('username',username);
+            // Store the username in localStorage for use throughout the quiz
+            localStorage.setItem('username',username);
 
-// Display a greeting message with the username..
-// document.getElementById('greeting').innerText = 'Hello, ${username}! Let's start the quiz.';});
+            // Display a greeting message with the username..
+            // document.getElementById('greeting').innerText = 'Hello,${username}! Let's start the quiz.';});
 
-// Redirect to questions page
-window.location.href = 'questions.html';
+            // Redirect to questions page
+            window.location.href = 'questions.html';
+            });
+        }
+    else if (page === 'questions') {
+        runQuestionsPage()
+        console.log('This is Page 2');
+    }
 });
 
-let arrayquestionImages = ["assets/images/cirrus6_big.jpg", "assets/images/cirrocumulus1_big.jpg", "assets/images/cirrostratus1_big.jpg", "assets/images/altocumulus1_big.jpg",
-"assets/images/altostratus3_big.jpg", "assets/images/stratus2_big.jpg", "assets/images/stratocumulus1_big.jpg", "assets/images/nimbostratus1_big.jpg", "assets/images/cumulus3_big.jpg",
-"assets/images/cumulonimbus3_big.jpg"]
+function runQuestionsPage() {
+    document.getElementById("option0").innerHTML = "Hello Sian!";
+    let random_cloud = getRandomCloud()
+    console.log("random cloud: "+  random_cloud);
+    document.getElementById("image-question").src =
+questionImages[random_cloud];
+    let answers = generateAnswersArray(random_cloud, 4);
+    console.log("current cloud is "+ random_cloud);
+    console.log("answers: "+ answers);
 
-let correctAnswers = ["Cirrus", "Cirrocumulus", "Cirrostrattus", "Altcomulus", "Altostratus", "Stratus", "Stratocumulus", "Nimbostratus", "Cumulus", "Cumulonimbus"];
+    //update answer sections. (This could be done with a for loop, butas we always want four answers, this is fine
+    document.getElementById("option0").innerHTML = answers[0];
+    document.getElementById("option1").innerHTML = answers[1];
+    document.getElementById("option2").innerHTML = answers[2];
+    document.getElementById("option3").innerHTML = answers[3];
+}
+
+let questionImages = {
+  Cirrus: "assets/images/cirrus6_big.jpg",
+  Cirrocumulus: "assets/images/cirrocumulus1_big.jpg",
+  Cirrostrattus: "assets/images/cirrostratus1_big.jpg",
+  Altcomulus: "assets/images/altocumulus1_big.jpg",
+  Altostratus: "assets/images/altostratus3_big.jpg",
+  Stratus: "assets/images/stratus2_big.jpg",
+  Stratocumulus: "assets/images/stratocumulus1_big.jpg",
+  Nimbostratus: "assets/images/nimbostratus1_big.jpg",
+  Cumulus: "assets/images/cumulus3_big.jpg",
+  Cumulonimbus: "assets/images/cumulonimbus3_big.jpg",
+};
 
 let optionsArray = ["option0", "option1", "option2", "option3"];
 
-
-//This function counts how many times the next question button is pressed and therefore the question the user is on 
-// and it changes the HTML of the next-question button when the user is on the last question
-let i = 0;
-function questionCounter() {
-    i = i + 1;
-    if (i === 9){
-    document.getElementById('next-question').innerHTML = '<a href="finishing-page.html">Finish Quiz!</a>';
-    };
-    return i;
+function getRandomCloud(){
+    let cloud_types = Object.keys(questionImages);
+    console.log(cloud_types);
+    let cloud_num = Math.floor(Math.random()*cloud_types.length);
+    let cloud_type = cloud_types[cloud_num];
+    return cloud_type;
 }
 
-//this function loads the images in order after each click on the next-question button
-function whichQuestion(){
-    // where i is the count of clicks on next question button
-    document.getElementById("image-question").src = arrayquestionImages[i];
-}
-
-document.getElementById("startQuiz").addEventListener('click', whichQuestion);
-document.getElementById("startQuiz").addEventListener('click', correctAnswerPosition);
-document.getElementById("startQuiz").addEventListener('click', randomAnswers0);
-document.getElementById("startQuiz").addEventListener('click', randomAnswers1);
-document.getElementById("startQuiz").addEventListener('click', randomAnswers2);
-document.getElementById("startQuiz").addEventListener('click', randomAnswers3);
-
-document.getElementById("next-question").addEventListener('click', whichQuestion);
-document.getElementById("next-question").addEventListener('click', correctAnswerPosition);
-document.getElementById("next-question").addEventListener('click', randomAnswers0);
-document.getElementById("next-question").addEventListener('click', randomAnswers1);
-document.getElementById("next-question").addEventListener('click', randomAnswers2);
-document.getElementById("next-question").addEventListener('click', randomAnswers3);
-document.getElementById("next-question").addEventListener('click', questionCounter);
-
-//this function puts the correct answer in one of the options boxes
-
-function correctAnswerPosition(){
-    let randomPosition = Math.floor(Math.random()*4);
-    if (randomPosition === 0){
-        document.getElementById("option0").innerText = correctAnswers[i];
-    } else if (randomPosition === 1){
-        document.getElementById("option1").innerText = correctAnswers[i];
-    } else if (randomPosition === 2){
-        document.getElementById("option2").innerText = correctAnswers[i];
-    } else if (randomPosition === 3){
-        document.getElementById("option3").innerText = correctAnswers[i];
+function generateAnswersArray(correct_answer, number_of_answers) {
+    let answer_array = [correct_answer];
+    console.log("the current answer array is: " + answer_array);
+    copied_questionImages = {...questionImages};
+    console.log("copied_questions array: "+copied_questionImages);
+    delete copied_questionImages[correct_answer];
+    console.log("copied_questions array minus correct answer" +copied_questionImages);
+    let cloud_types = Object.keys(copied_questionImages);
+    console.log("cloud_types array:" + cloud_types);
+    for (i=0; i < number_of_answers -1; i++) {
+        cloud_num = Math.floor(Math.random()*cloud_types.length);
+        console.log("cloud_num:" + cloud_num);
+        chosen_cloud = cloud_types[cloud_num];
+        //scramble array by randomising array update method
+        rand_num = Math.random()
+        if (rand_num < 0.5) {
+            answer_array.push(chosen_cloud)
+        } else {
+            answer_array.unshift(chosen_cloud)
+        }
+        //Remove the element from array so it's not reselected
+        cloud_types.splice(cloud_num,1);
+        console.log("cloud_types post deletion: "+ cloud_types);
+        console.log(answer_array);
     }
+    return answer_array
 }
-
-//functions for randomising the positions of incorrect answers
-function randomAnswers0() {
-        if (document.getElementById("option0").innerText === ""){
-            document.getElementById("option0").innerText = correctAnswers[Math.floor(Math.random()*10)];
-            
-        } 
-}
-
-function randomAnswers1() {
-    k = Math.floor(Math.random()*9);
-    if (document.getElementById("option1").innerHTML === ""){
-        document.getElementById("option1").innerHTML = correctAnswers[k];
-        correctAnswers.splice(k, 1);
-    } 
-
-}
-
-function randomAnswers2() {
-    k = Math.floor(Math.random()*8)
-    if (document.getElementById("option2").innerHTML === ""){
-        document.getElementById("option2").innerHTML = correctAnswers[k];
-        correctAnswers.splice(k, 1);
-    } 
-}
-
-function randomAnswers3() {
-    k = Math.floor(Math.random()*7)
-    if (document.getElementById("option3").innerHTML === ""){
-        document.getElementById("option3").innerHTML = correctAnswers[k];
-}
-}
-function resetArray() {
-    correctAnswers = ["Cirrus", "Cirrocumulus", "Cirrostrattus", "Altcomulus", "Altostratus", "Stratus", "Stratocumulus", "Nimbostratus", "Cumulus", "Cumulonimbus"];
-}
-
-//this function resets the eventlisteners after the next-question button is clicked
-document.getElementById("next-question").addEventListener("click", resetEventListeners);
-function resetEventListeners() {
-document.getElementById("next-question").addEventListener("click", randomAnswers0);
-document.getElementById("next-question").addEventListener("click", randomAnswers1);
-document.getElementById("next-question").addEventListener("click", randomAnswers2);
-document.getElementById("next-question").addEventListener("click", randomAnswers3);
-}
-
-//These functions check each option for correctness and add to either the correct or incorrect answers tally
-let correctlyAnswered = 0;
-let incorrectlyAnswered = 0;
-function checkAnswer0(){
-    document.getElementById("option0").removeEventListener("click", checkAnswer3);
-    if (document.getElementById("option0").innerText === correctAnswers[i]){
-        correctlyAnswered = correctlyAnswered + 1;
-
-        return correctlyAnswered;
-    } else {
-        incorrectlyAnswered = incorrectlyAnswered + 1;
-        return incorrectlyAnswered;
-    }
-}
-
-function checkAnswer1(){
-    document.getElementById("option1").removeEventListener("click", checkAnswer3);
-    if (document.getElementById("option1").innerText === correctAnswers[i]){
-        correctlyAnswered = correctlyAnswered + 1;
-        return correctlyAnswered;
-    } else {
-        incorrectlyAnswered = incorrectlyAnswered + 1;
-        return incorrectlyAnswered;
-    }
-}
-
-function checkAnswer2(){
-    document.getElementById("option2").removeEventListener("click", checkAnswer3);
-    if (document.getElementById("option2").innerText === correctAnswers[i]){
-        correctlyAnswered = correctlyAnswered + 1;
-        return correctlyAnswered;
-    } else {
-        incorrectlyAnswered = incorrectlyAnswered + 1;
-        return incorrectlyAnswered;
-    }
-}
-
-function checkAnswer3(){
-    document.getElementById("option3").removeEventListener("click", checkAnswer3)
-    if (document.getElementById("option3").innerText === correctAnswers[i]){
-        correctlyAnswered = correctlyAnswered + 1;
-        return correctlyAnswered;
-    } else {
-        incorrectlyAnswered = incorrectlyAnswered + 1;
-        return incorrectlyAnswered;
-    }
-}
-
-document.getElementById("next-question").addEventListener("click", resetArray);
-
-function scoreTally(){
-    
-}
-
-
